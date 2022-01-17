983fd8e6722f6beb7b4a16b39ea046b8ce206f85
jesielviana <jesielviana@pm.me>
2022-01-17 17:11:13 -0300

    update gitignore

--- env.bat
+++ env.bat
@@ -1,3 +1,3 @@
-@set VUFIND_HOME=/Users/jesielviana/dev/ibict/ibict-vufind
-@set VUFIND_LOCAL_DIR=/Users/jesielviana/dev/ibict/ibict-vufind/local
+@set VUFIND_HOME=/Users/jesielviana/dev/ibict/oasisbr-vufind
+@set VUFIND_LOCAL_DIR=/Users/jesielviana/dev/ibict/oasisbr-vufind/local
 @set VUFIND_LOCAL_MODULES=Oasisbr
