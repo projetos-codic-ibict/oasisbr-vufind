030c884c00c6509caaa32dad680f9610980e8671
Jesiel Viana <jesielviana@proton.me>
2024-10-30 12:18:58 -0300

    Delete cache/cli/configs/e23d7b15c8d71ff439d79be608d9ea12 directory

--- cache/cli/configs/e23d7b15c8d71ff439d79be608d9ea12/module-classmap-cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'VuFindTheme\\Module' => '/home/jesielviana/dev/ibict/BDTD/bdtd-vufind-nova/module/VuFindTheme/Module.php',
-  'VuFindSearch\\Module' => '/home/jesielviana/dev/ibict/BDTD/bdtd-vufind-nova/module/VuFindSearch/Module.php',
-  'VuFind\\Module' => '/home/jesielviana/dev/ibict/BDTD/bdtd-vufind-nova/module/VuFind/Module.php',
-  'VuFindAdmin\\Module' => '/home/jesielviana/dev/ibict/BDTD/bdtd-vufind-nova/module/VuFindAdmin/Module.php',
-  'VuFindApi\\Module' => '/home/jesielviana/dev/ibict/BDTD/bdtd-vufind-nova/module/VuFindApi/Module.php',
-  'VuFindConsole\\Module' => '/home/jesielviana/dev/ibict/BDTD/bdtd-vufind-nova/module/VuFindConsole/Module.php',
-);
