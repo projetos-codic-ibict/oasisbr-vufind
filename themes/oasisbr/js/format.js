c9a9c0df4760b0547b97c3b5422c84af26214290
Jesiel Viana <jesielviana@proton.me>
2025-07-16 10:40:51 -0300

    corrigindo formatacao para numeros invalidos

--- themes/oasisbr/js/format.js
+++ themes/oasisbr/js/format.js
@@ -1,4 +1,8 @@
 function formatNumber(value) {
+  const number = Number(value);
+  if (isNaN(number)) {
+    return '';
+  }
   const formatPtBr = new Intl.NumberFormat('pt-BR');
-  return formatPtBr.format(value);
+  return formatPtBr.format(number);
 }
